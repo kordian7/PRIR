6326690546ed81388ea231a5f58228a92d020dc5
Kordian Kurdziel <kordiankurdziel@gmail.com>
2018-10-17 00:53:16 +0200

    Gauss fixed

--- lab1/gauss_omp.cpp
+++ lab1/gauss_omp.cpp
@@ -7,22 +7,33 @@ using namespace cv;
 using namespace std;
 
 const int maskSize = 5;
-const int mask[maskSize][maskSize] =    {{1, 1, 1, 1, 1},
-                                        {1, 1, 1, 1, 1},
-                                        {1, 1, 1, 1, 1},
-                                        {1, 1, 1, 1, 1},
-                                        {1, 1, 1, 1, 1}};
+const int mask[maskSize][maskSize] =    {
+										{1, 1, 2, 1, 1},
+                                        {1, 2, 4, 2, 1},
+                                        {1, 4, 8, 4, 2},
+                                        {1, 2, 4, 2, 1},
+                                        {1, 1, 2, 1, 1}};
+int maskWeight;
+int margin;
 
 int calculateNewPixelChannelValue(Mat channel, int row, int col) {
     int sum = 0;
-    int maskWeight = 0;
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
+    for (int i = 0; i < maskSize; ++i) {
+        for (int j = 0; j < maskSize; ++j) {
             sum+= mask[i][j] * ((int) channel.at<uchar>(row + i - 2, col + j - 2));
-            maskWeight += mask[i][j];
         }
     }
     return (int) (sum / maskWeight);
+}
+
+void initMask() {
+	margin = maskSize / 2;
+	maskWeight = 0;
+    for (int i = 0; i < maskSize; ++i) {
+        for (int j = 0; j < maskSize; ++j) {
+            maskWeight += mask[i][j];
+        }
+    }
 }
 
 int main(int argc, char** argv )
@@ -33,38 +44,39 @@ int main(int argc, char** argv )
         return -1;
     }
 	int threadsNumber;
+	double startTime, finishTime;
 	threadsNumber= strtol(argv[1], NULL, 10);
-	omp_set_num_threads(threadsNumber);
-    Mat img;
-    Mat outputImg;
-    img = imread( argv[2], 1 );
-    if ( !img.data )
+	string srcImagePath = argv[2];
+	string dstImagePath = argv[3];
+
+    Mat inputImg, outputImg;
+    inputImg = imread( srcImagePath, CV_LOAD_IMAGE_COLOR);
+    if ( !inputImg.data )
     {
         printf("Problem with image \n");
         return -1;
     }
     vector<Mat> rgbInputChannels;
     vector<Mat> rgbOutputChannels;
-    split(img, rgbInputChannels);
-    split(img, rgbOutputChannels);
+    split(inputImg, rgbInputChannels);
+    split(inputImg, rgbOutputChannels);
 
-	int margin = maskSize / 2;
+	initMask();
 
-	auto startTime = std::chrono::high_resolution_clock::now();
+    startTime = omp_get_wtime();
 	int i,j;
-	#pragma omp parallel for private(i,j) schedule(dynamic)
-    for (i = margin; i < img.rows - margin; i++) {
-        for (j = margin; j < img.cols - margin; j++) {
+	#pragma omp parallel for default(shared) private(i,j) schedule(runtime) num_threads(threadsNumber)
+    for (i = margin; i < inputImg.rows - margin; i++) {
+        for (j = margin; j < inputImg.cols - margin; j++) {
             rgbOutputChannels[0].at<uchar>(i,j) = calculateNewPixelChannelValue(rgbInputChannels[0], i, j);
             rgbOutputChannels[1].at<uchar>(i,j) = calculateNewPixelChannelValue(rgbInputChannels[1], i, j);
             rgbOutputChannels[2].at<uchar>(i,j) = calculateNewPixelChannelValue(rgbInputChannels[2], i, j);
         }
     }
-	auto finishTime = std::chrono::high_resolution_clock::now();
-	auto msDuration = std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime);
-	cout << "Czas: " << msDuration.count() << "ms\n"	;
+    finishTime = omp_get_wtime();
+	cout << "Czas: " << ((finishTime - startTime) * 1000) << "ms\n"	;
     merge(rgbOutputChannels, outputImg);
-    imwrite(argv[3], outputImg);
+    imwrite(dstImagePath, outputImg);
     waitKey(0);
     return 0;
 }
