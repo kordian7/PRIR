b9065b6c897796edb54344ba71c4a20b1a7daaea
Kordian Kurdziel <kordiankurdziel@gmail.com>
2018-10-24 20:15:40 +0200

    Lab2 fixed

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -61,8 +61,15 @@ Mat cutPicture(Mat picture, int x, int y, int width, int height){
     cout << "CUT w: " << width << endl;
     cout << "CUT h: " << height << endl;
 
+	cv::Rect myROI(x, y, width, height);
 
-    return picture(Rect(x, y, width, height)).clone();
+	cv::Mat croppedRef(picture, myROI);
+	cv::Mat cropped;
+	croppedRef.copyTo(cropped);
+	
+    cout << "cropped img rows: " << cropped.rows << endl;
+    cout << "cropped img cols: " << cropped.cols << endl; 
+    return cropped;
 }
 
 Mat gauss(Mat img) {
@@ -73,18 +80,20 @@ Mat gauss(Mat img) {
     split(img, rgbInput);
     split(img, rgbOutput);
 
-    cout << "img rows: " << img.rows << endl;
-    cout << "img cols: " << img.cols << endl; 
+    cout << "img1 rows: " << img.rows << endl;
+    cout << "img1 cols: " << img.cols << endl; 
 
-    for (int i = 0; i < img.rows; ++i) {
-        for (int j = 0; j < img.cols; ++j) {
+    for (int i = margin; i < img.rows - margin; ++i) {
+        for (int j = margin; j < img.cols - margin; ++j) {
             rgbOutput[0].at<uchar>(i,j) = calculateNewPixelChannelValue(rgbInput[0], i, j);
             rgbOutput[1].at<uchar>(i,j) = calculateNewPixelChannelValue(rgbInput[1], i, j);
             rgbOutput[2].at<uchar>(i,j) = calculateNewPixelChannelValue(rgbInput[2], i, j);
         }
     }
-
     merge(rgbOutput, output);
+
+	cout << "img2 rows: " << output.rows << endl;
+    cout << "img2 cols: " << output.cols << endl; 
     return output;
 }
                                         
@@ -95,6 +104,7 @@ int main(int argc, char** argv )
     int rank, numberOfProcesses;
     double startTime, finishTime;
     int x, y, imgSplitSize;
+	initMask();
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank (commmunicate, &rank);
@@ -116,6 +126,7 @@ int main(int argc, char** argv )
 
         cout << "ORIGINAL IMAGE ROWS: " << inputImg.rows << endl;
         cout << "ORIGINAL IMAGE COLS: " << inputImg.cols << endl;
+
 
 
         if ( !inputImg.data )
@@ -154,9 +165,9 @@ int main(int argc, char** argv )
 
         // Get filtered parts and merge to image
         for(int i = 1; i < numberOfProcesses; i++) {
-            receive(&x, 1, MPI_INT, i, MULTIPLY_ID*i+START_VALUE_TAG);
-            receive(&y, 1, MPI_INT, i, MULTIPLY_ID*i+END_VALUE_TAG);
-            partOfImg = Mat(y, x, CV_8UC3);
+            //receive(&x, 1, MPI_INT, i, MULTIPLY_ID*i+START_VALUE_TAG);
+            //receive(&y, 1, MPI_INT, i, MULTIPLY_ID*i+END_VALUE_TAG);
+            partOfImg = Mat(x, y, CV_8UC3);
 
             receive(partOfImg.data, x*y*3, MPI_BYTE, i, MULTIPLY_ID*i+DATA_TAG);
             outputImg.push_back(partOfImg);
@@ -171,14 +182,11 @@ int main(int argc, char** argv )
         cout << "RECEIVE START: " << x << endl;
         cout << "RECEIVE END: " << y << endl;
 
-        
-        partOfImg = Mat(0, 0, CV_8UC3);
+        partOfImg = Mat(x, y, CV_8UC3);
         receive(partOfImg.data, x * y * 3, MPI_BYTE, MASTER_PROCESS, DATA_TAG);
 
-        cout << "RECEIVE IMAGE: " << partOfImg.cols << endl;
-
         partOfImg = gauss(partOfImg);
-
+    
         send(&x, 1, MPI_INT, MASTER_PROCESS, MULTIPLY_ID * rank + START_VALUE_TAG);
         send(&y, 1, MPI_INT, MASTER_PROCESS, MULTIPLY_ID * rank + END_VALUE_TAG);
         send(partOfImg.data, x*y*3, MPI_BYTE, MASTER_PROCESS, MULTIPLY_ID * rank + DATA_TAG);
